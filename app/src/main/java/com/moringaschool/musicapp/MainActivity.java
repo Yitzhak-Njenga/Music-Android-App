e9d80aaa01dda487fa973f3dc1939b60dc767835
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-01-09 20:33:06 +0300

    added a variariables for ui design

--- app/src/main/java/com/moringaschool/musicapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/musicapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.moringaschool.musicapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //reference to UI elements
+
+    LinearLayout L1,L2;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        L1 =(LinearLayout)findViewById(R.id.l1);
+        L2 =(LinearLayout)findViewById(findViewById(R.id.l2);
+
+        tv =(TextView)findViewById(R.id.tag);
     }
 }
