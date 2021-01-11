00a1056eaa79e11a2f324ec740437a6a5bd39842
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-01-11 16:41:52 +0300

    added a toast for register button

--- app/src/main/java/com/moringaschool/musicapp/MusicActivity.java
+++ app/src/main/java/com/moringaschool/musicapp/MusicActivity.java
@@ -2,10 +2,12 @@ package com.moringaschool.musicapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,15 +19,23 @@ public class MusicActivity extends AppCompatActivity {
     @BindView(R.id.register_button) Button registerButton;
     @BindView(R.id.login_button) Button loginButton;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+        ButterKnife.bind(this);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MusicActivity.this,"Register",Toast.LENGTH_LONG).show();
+            }
+        });
+
 
     }
-
-
-
-
 }
+
+
 
